52507020e59246e63befeb20d501f1fe741b12c1
Moritz Hamann <mohikaner@quantentunnel.de>
2013-03-18 22:29:31 +0100

    changed javascript of the settings page to work properly with ajax calls

--- assets/javascripts/settings.js
+++ assets/javascripts/settings.js
@@ -2,28 +2,13 @@ function updateStatustransitionList(event, data, status, xhr){
   $("#status-transition-list").html(data);
 };
 
-function generateLink(){
-  var href = $("#link-add-transition").attr("href");
-  var from = $("#new-transition-from").attr("value");
-  var to = $("#new-transition-to").attr("value");
-  return href + "from_id=" + from + "&to_id=" + to;
-}
-
-function applyCallbacks(){
-  $("#status-transition-list").live("ajax:success", updateStatustransitionList);
-  
-  
-
-  $("#link-add-transition").click(function(){
-    var href = $("#link-add-transition").attr("href");
-    var from = $("#new-transition-from").attr("value");
-    var to = $("#new-transition-to").attr("value");
-    $(this).attr("href","");
-    $(this).attr("href", href + "from_id=" + from + "&to_id=" + to);
-  })
-};
-
 $(document).ready(function(){
-  applyCallbacks();
-  console.log("callbacks activated");
+  $(document).on("ajax:success", "#status-transition-list", updateStatustransitionList)
+  $(document).on("change", "[id^=new-transition]", function(){
+    var href = $("#link-add-transition").prop("href");
+    var from = $("#new-transition-from").prop("value");
+    var to = $("#new-transition-to").prop("value");
+
+    $("#link-add-transition").prop("href", href + "&from_id=" + from + "&to_id=" + to);
+  });
 });
